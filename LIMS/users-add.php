3fac705cfcac16e8b1a3f2d608cc4e4d66f480d7
DoTienOanh <oanhvy2bg@gmail.com>
2025-02-22 23:49:42 +0700

    User page - video 8

--- LIMS/users-add.php
+++ LIMS/users-add.php
@@ -4,6 +4,8 @@
     if(!isset($_SESSION['user'])) header('location: login.php');
     $_SESSION['table'] = 'users';
     $user =$_SESSION['user'];
+    $users = include('database/show-users.php');
+    
 ?>
 
 <!DOCTYPE html>
@@ -24,39 +26,76 @@
             <?php include('partials/app-topnav.php') ?>    
             <div class="dashboard_content">
                 <div class="dashboard_content_main">
-                    <div id="userAddFormContainer">  
-                        <form action="database/add.php" method="POST" class="appForm">
-                            <div class="appFormInputContainer">
-                                <label for="first_name">First Name</label>
-                                <input type="text" class="appFormInput" id="first_name" name="first_name" />
-                            </div>
-                            <div class="appFormInputContainer">
-                                <label for="last_name">Last Name</label>
-                                <input type="text" class="appFormInput" id="last_name" name="last_name" />
-                            </div>
-                            <div class="appFormInputContainer">
-                                <label for="email">Email</label>
-                                <input type="text" class="appFormInput" id="email" name="email" />
-                            </div>
-                            <div class="appFormInputContainer">
-                                <label for="password">Password</label>
-                                <input type="password" class="appFormInput" id="password" name="password" />
-                            </div>
-                            
-                            <button type="submit" class="appBtn"><i class="fa fa-plus"></i>Add User</button>
-                        </form>
-                        <?php 
-                            if(isset($_SESSION['response'])) { 
-                            $response_message = $_SESSION['response']['message'];
-                            $is_success = $_SESSION['response']['success'];
+                    <div class="row">
+                        <div class="column column-5">
+                            <h1 class="section_header"><i class="fa fa-plus"></i> Create User</h1>
+                            <div id="userAddFormContainer">  
+                                <form action="database/add.php" method="POST" class="appForm">
+                                    <div class="appFormInputContainer">
+                                        <label for="first_name">First Name</label>
+                                        <input type="text" class="appFormInput" id="first_name" name="first_name" />
+                                    </div>
+                                    <div class="appFormInputContainer">
+                                        <label for="last_name">Last Name</label>
+                                        <input type="text" class="appFormInput" id="last_name" name="last_name" />
+                                    </div>
+                                    <div class="appFormInputContainer">
+                                        <label for="email">Email</label>
+                                        <input type="text" class="appFormInput" id="email" name="email" />
+                                    </div>
+                                    <div class="appFormInputContainer">
+                                        <label for="password">Password</label>
+                                        <input type="password" class="appFormInput" id="password" name="password" />
+                                    </div>
+                                    
+                                    <button type="submit" class="appBtn"><i class="fa fa-plus"></i>Add User</button>
+                                </form>
+                                <?php 
+                                    if(isset($_SESSION['response'])) { 
+                                    $response_message = $_SESSION['response']['message'];
+                                    $is_success = $_SESSION['response']['success'];
 
-                        ?>
-                            <div class="responseMessage">
-                                <p class="responseMessage <?= $is_success ? 'responseMessage__success' : 'responseMessage__error' ?>">
-                                    <?= $response_message ?>
-                                </p>
+                                ?>
+                                    <div class="responseMessage">
+                                        <p class="responseMessage <?= $is_success ? 'responseMessage__success' : 'responseMessage__error' ?>">
+                                            <?= $response_message ?>
+                                        </p>
+                                    </div>
+                                <?php unset($_SESSION['response']); }  ?>
                             </div>
-                        <?php unset($_SESSION['response']); }  ?>
+                        </div>
+                        <div class="column column-7">
+                            <h1 class="section_header"><i class="fa fa-list"></i> List of Users</h1>
+                            <div class="section_content">
+                                <div class="users">
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>First Name</th>
+                                                <th>Last Name</th>
+                                                <th>Email</th>
+                                                <th>Created At</th>
+                                                <th>Update At</th>
+                                            <tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php foreach($users as $index =>$user) { ?>
+                                                <tr>
+                                                    <td><?= $index + 1 ?></td>
+                                                    <td><?= $user['first_name'] ?></td>
+                                                    <td><?= $user['last_name'] ?></td>
+                                                    <td><?= $user['email'] ?></td>
+                                                    <td><?= date('M d,Y 0 h:i:s A', strtotime($user['created_at'])) ?></td>
+                                                    <td><?= date('M d,Y 0 h:i:s A', strtotime($user['updated_at'])) ?></td>
+                                                </tr>
+                                            <?php } ?> 
+                                        </tbody>  
+                                    </table>
+                                    <p class="userCount"><?= count($users) ?> Users</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
